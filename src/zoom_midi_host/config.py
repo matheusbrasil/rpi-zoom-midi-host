f71e78b0a5d71dbc9e45f299b0876871c275dc86
Matheus Brasil <matheus.brasil@gmail.com>
2025-10-05 15:07:42 +0100

    feat fix

--- src/zoom_midi_host/config.py
+++ src/zoom_midi_host/config.py
@@ -21,7 +21,13 @@ PEDAL_MODELS: Dict[str, PedalModel] = {
         name="Zoom MS-60B+",
         vendor_id=0x1686,
         product_ids=(0x01AD, 0x01AE, 0x07A1),
-        midi_port_keywords=("ZOOM MS-60B+", "MS-60B+"),
+        midi_port_keywords=(
+            "ZOOM MS-60B+",
+            "MS-60B+",
+            "ZOOM MS Plus",
+            "MS Plus Series",
+            "MS Plus",
+        ),
     ),
 }
 
